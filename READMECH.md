0b53485fff0af8169ecc49c55c7d7d6260f700e1
0_@ <105268030+0a1te@users.noreply.github.com>
2024-05-05 23:12:56 +0800

    update readme

--- READMECH.md
+++ READMECH.md
@@ -9,19 +9,15 @@
 ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
 ```
 
-- why?
+- 为什么会有这个代码仓?
 
-> Hi, there! I'm learning about program analysis recently. 
+> 苦于程序分析中对于编译原理的浅薄理解, 特别是在看到一本业内称为猴书的解释器开发书籍后, 我决定跟着写一个自己的解释器, 并在其基础上做一些有趣的拓展。
 > 
-> However, I knew so little about the Compiler. Until I discovered the interpreter development book called the Monkey Book in the industry, I decided to start learning relevant knowledge through a small project and expand interesting functions.
-
-> Therefore, this is an interpreter developed using go. Since I am not sure how far it will be developed in the future, I call it Pandora's Box to express my hope and alert myself...
+> 所以, **这是一款使用go开发的解释器**, 由于不确定未来会开发到何种程度, 我称之为潘多拉魔盒, 以表示希冀并警醒自己...
 
 
 
-
-## DevNotion
-
+## 开发日志
 
 ### Token
 
@@ -55,4 +51,5 @@ type Lexer struct {
 ```
 
 2. REPL
+
 
--- main.go
+++ main.go
@@ -4,24 +4,31 @@ import (
 	"Pandora_Box/repl"
 	"fmt"
 	"os"
-	user2 "os/user"
 )
+
+const banner = `	██████╗  █████╗ ███╗   ██╗██████╗  ██████╗ ██████╗  █████╗         ██████╗  ██████╗ ██╗  ██╗
+	██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔═══██╗██╔══██╗██╔══██╗        ██╔══██╗██╔═══██╗╚██╗██╔╝
+	██████╔╝███████║██╔██╗ ██║██║  ██║██║   ██║██████╔╝███████║        ██████╔╝██║   ██║ ╚███╔╝ 
+	██╔═══╝ ██╔══██║██║╚██╗██║██║  ██║██║   ██║██╔══██╗██╔══██║        ██╔══██╗██║   ██║ ██╔██╗ 
+	██║     ██║  ██║██║ ╚████║██████╔╝╚██████╔╝██║  ██║██║  ██║███████╗██████╔╝╚██████╔╝██╔╝ ██╗
+	╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝`
 
 func init() {
 	// banner
-	fmt.Println("this is banner")
+	fmt.Println(banner)
 }
 
 func main() {
 
-	user, err := user2.Current()
-	if err != nil {
-		panic(err)
-	}
+	// user, err := user2.Current()
+	// if err != nil {
+	//	panic(err)
+	//}
 
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
-		user.Username)
+	// fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 
-	fmt.Printf("Feel free to type in commands\n")
+	// fmt.Printf("Feel free to type in commands\n")
+
 	repl.Start(os.Stdin, os.Stdout)
+
 }
